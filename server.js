e171df45b2cf1544055de661254a73cb310a110a
kaminskimarcin <33075147+kaminskimarcin@users.noreply.github.com>
2019-12-16 17:37:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const path = require('http');
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/hakatonfrontend'));
 
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/barcodefront/index.html'));
+res.sendFile(path.join(__dirname));
 });
 
 // Start the app by listening on the default Heroku port
