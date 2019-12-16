660123486439f8caa32d4be1a905f8555b761a72
kaminskimarcin <33075147+kaminskimarcin@users.noreply.github.com>
2019-12-16 17:48:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const http = require('http');
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/hakatonfrontend'));
+app.use(express.static(__dirname + '/dist/barcodefront'));
 
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname));
