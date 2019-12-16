77f0780242e8597b7988ad3cdeccc60101486a53
kaminskimarcin <33075147+kaminskimarcin@users.noreply.github.com>
2019-12-16 17:11:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 //Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
-
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/hakatonfrontend'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/hakatonfrontend/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
