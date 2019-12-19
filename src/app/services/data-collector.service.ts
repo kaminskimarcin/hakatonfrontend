084737a8180bc2527bcd8e3eb2747b86dd47fd82
kaminskimarcin <kaminski283@gmail.com>
2019-12-19 23:38:15 +0100

    Fixed variables naming

--- src/app/services/data-collector.service.ts
+++ src/app/services/data-collector.service.ts
@@ -1,32 +1,32 @@
-import {Injectable} from "@angular/core";
-import {Item} from "../model/items.model";
+import {Injectable} from '@angular/core';
+import {Item} from '../model/items.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataCollectorService {
-  private _items: Array<Item>;
-  private _orderId: number = 0;
+  private items: Array<Item>;
+  private orderId = 0;
 
 
   getItems(): Array<Item> {
-    if (this._items === undefined) {
+    if (this.items === undefined) {
       return new Array<Item>();
     } else {
-      return this._items;
+      return this.items;
     }
   }
 
   setItems(value: Array<Item>) {
-    this._items = value;
+    this.items = value;
   }
 
   getOrderId(): number {
-    return this._orderId;
+    return this.orderId;
   }
 
   setOrderId(value: number) {
-    this._orderId = value;
+    this.orderId = value;
   }
 
 }
